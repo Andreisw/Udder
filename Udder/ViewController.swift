adaf6179e8df8786accb818ca05a28006fcd1ac0
Your Name <andreigbcojocaru@gmail.com>
2021-01-28 21:25:16 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Andrei Cojocaru on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
